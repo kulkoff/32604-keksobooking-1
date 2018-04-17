f22f842883a70ada5ecd555317649f71283d0ca5
alex.kulkoff <alex.kulkoff@gmail.com>
2018-04-17 17:18:07 +0500

    Добавил файлы

---
+++ js/data.js
@@ -0,0 +1,105 @@
+// 'use strict';
+//
+// var types = ['palace', 'flat', 'house', 'bungalo'];
+// var titles = [
+//   'Большая уютная квартира',
+//   'Маленькая неуютная квартира',
+//   'Огромный прекрасный дворец',
+//   'Маленький ужасный дворец',
+//   'Красивый гостевой домик',
+//   'Некрасивый негостеприимный домик',
+//   'Уютное бунгало далеко от моря',
+//   'Неуютное бунгало по колено в воде'
+// ];
+// var minPrice = 1000;
+// var maxPrice = 1000000;
+// var roomsMin = 1;
+// var roomsMax = 5;
+// var minGuest = 1;
+// var maxGuest = 6;
+// var times = ['12-00', '13-00', '14-00'];
+// var featuresList = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+// var featuresMin = 0;
+// var minXLocation = 300;
+// var maxXLocation = 900;
+// var minYLocation = 100;
+// var maxYLocation = 500;
+// var photoLinks = [
+//   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+//   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+//   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+// ];
+//
+// // Функция генерации объекта
+// function createPinData(id) {
+//   var xCoord = getRandomNumber(minXLocation, maxXLocation);
+//   var yCoord = getRandomNumber(minYLocation, maxYLocation);
+//
+//   return {
+//     author: {
+//       avatar: 'img/avatars/user0' + id + '.png'
+//     },
+//     offer: {
+//       title: titles.shift(),
+//       address: xCoord + ', ' + yCoord,
+//       price: getRandomNumber(minPrice, maxPrice),
+//       type: getRandomNumberOfArray(types),
+//       rooms: getRandomNumber(roomsMin, roomsMax),
+//       guests: getRandomNumber(minGuest, maxGuest),
+//       checkin: getRandomNumberOfArray(times),
+//       checkout: getRandomNumberOfArray(times),
+//       features: makeRandomLengthArray(featuresList),
+//       description: '',
+//       photos: getRandomNumberOfArray(photoLinks)
+//     },
+//     location: {
+//       x: xCoord,
+//       y: yCoord
+//     }
+//   };
+// }
+//
+// // Функция генерации объявления
+// function renderAdvertOnMap(pinData) {
+//   var articleTemplate = document.querySelector('template').content.querySelector('article');
+//   var advert = articleTemplate.cloneNode(true);
+//   advert.querySelector('.popup__title').textContent = pinData.offer.title;
+//   advert.querySelector('.popup__text--address').textContent = pinData.offer.address;
+//   advert.querySelector('.popup__text--price').innerHTML = pinData.offer.price + ' &#x20bd;/ночь';
+//
+//   var typeOfAccommodation;
+//   if (pinData.offer.type === 'flat') {
+//     typeOfAccommodation = 'Квартира';
+//   } else if (pinData.offer.type === 'bungalo') {
+//     typeOfAccommodation = 'Бунгало';
+//   } else {
+//     typeOfAccommodation = 'Дом';
+//   }
+//
+//   advert.querySelector('.popup__type').textContent = typeOfAccommodation;
+//   advert.querySelector('.popup__text--capacity').textContent = pinData.offer.rooms + ' комнаты' + ' для ' + pinData.offer.guests + ' гостей';
+//   advert.querySelector('.popup__text--time').textContent = 'Заезд после ' + pinData.offer.checkin + ', выезд до ' + pinData.offer.checkout;
+//   var listItems = [];
+//   for (var j = 0; j < pinData.offer.features.length; j++) {
+//     listItems.push('<li class="feature feature--' + pinData.offer.features[j] + '"></li>');
+//   }
+//   advert.querySelector('.popup__features').innerHTML = listItems.join(' ');
+//   advert.querySelector('.popup__avatar').src = pinData.author.avatar;
+//   advert.querySelector('.popup__photo').src = pinData.offer.photos;
+//
+//   return advert;
+// }
+//
+//
+// // Функции рандомайзеры
+// function getRandomNumber(min, max) {
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
+//
+// function getRandomNumberOfArray(array) {
+//   return array[Math.floor(Math.random() * array.length)];
+// }
+//
+// function makeRandomLengthArray(array) {
+//   return array.slice(getRandomNumber(featuresMin, featuresList.length));
+// }
