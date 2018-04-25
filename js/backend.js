7a151be33c2042b31babfb9606090d2cb41f3290
alex.kulkoff <alex.kulkoff@gmail.com>
2018-04-25 16:45:11 +0500

    Закинул в функцию xhr

--- js/backend.js
+++ js/backend.js
@@ -1,56 +1,42 @@
 'use strict';
 
 (function () {
+
   window.backend = {
     save: function (data, onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выплниться за ' + xhr.timeout + ' мс');
-      });
-
-      xhr.timeout = 10000;
-
+      var xhr = request(onLoad, onError);
       xhr.open('POST', 'https://js.dump.academy/keksobooking');
       xhr.send(data);
     },
     load: function (onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выплниться за ' + xhr.timeout + ' мс');
-      });
-
-      xhr.timeout = 10000;
+      var xhr = request(onLoad, onError);
 
       xhr.open('GET', 'https://js.dump.academy/keksobooking/data');
       xhr.send();
     }
   };
+
+  function request(onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.timeout = 10000;
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выплниться за ' + xhr.timeout + ' мс');
+    });
+
+    return xhr;
+  }
+
 })();
